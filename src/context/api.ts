00c388a3ef1d25f42b4be9cfeb56f3ff3ec1d4f1
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2025-07-19 20:33:55 +0500

    Update api.ts

--- src/context/api.ts
+++ src/context/api.ts
@@ -2,6 +2,6 @@
 
 // src/services/api.ts
 
-const BASE_URL = 'https://email-server-production-941a.up.railway.app';
+const BASE_URL = 'https://web-production-4958b.up.railway.app/';
 
 export const API_URL = `${BASE_URL}/api`;
