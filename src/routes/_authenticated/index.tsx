061faf1fc2b25f99c61349c4effe1b68325bbd47
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2025-11-01 12:16:03 +0500

    Replace Tasks component with Dashboard component

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
-import Tasks from '@/features/tasks'
+// import Tasks from '@/features/tasks'
+import Dashboard from '@/features/dashboard'
 
 export const Route = createFileRoute('/_authenticated/')({
-  component: Tasks,
+  component: Dashboard,
 })
